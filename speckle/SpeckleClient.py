34b18cc1e28246e759a8f57f6947e38e98ae529b
Sean <seanmerrifield@gmail.com>
2019-02-18 22:12:48 +0100

    Added Revit Stream

--- speckle/SpeckleClient.py
+++ speckle/SpeckleClient.py
@@ -386,6 +386,23 @@ class SpeckleApiClient():
         # return None   
         return r.json()      
 
+    def StreamAddObjectsAsync(self, streamId, object_list):
+        """
+        Add objects to stream
+        """ 
+        assert streamId is not None
+        assert type(object_list) == list
+
+        url = self.server + "/streams/%s" % streamId
+
+        objects = [{'_id': objectId, "type": "Placeholder"} for objectId in object_list]
+        r = self.session.put(url, json.dumps(objects))
+        if self.check_response_status_code(r):
+            return r.json()
+        # return None   
+        return r.json()  
+  
+
     def UserGetAsync(self):
         url = self.server + "/accounts"
         r = self.session.get(url)
---
+++ speckle/revit_stream.py
@@ -0,0 +1,81 @@
+
+from .SpeckleClient import SpeckleApiClient
+s = SpeckleApiClient()
+
+#Grab Project Name
+
+#Grab Username
+username = "test@arup.com"
+
+#Grab Revit analytical geometry and properties
+
+#for every analytical element
+for el in elements:
+
+    #Fill in object properties from the element
+    obj_props = {
+        "owner": username
+        'type': "Mesh", 
+        "name": "RevitFamily", 
+        'applicationId': "RevitID", 
+        'vertices': [], 
+        'faces' = [], 
+        'properties': {} , 
+        
+        }
+
+    obj = {
+            "private": False,
+            "anonymousComments": True,
+            "canRead": [],
+            "canWrite": [],
+            "comments": [],
+            "deleted": False,
+            "hash": "hash",
+            "geometryHash": "Type.hash",
+            "parent": None,
+            "children": [],
+            "ancestors": [],
+            "transform": []
+        }
+
+    obj.extend(obj_props)
+
+    objects.append(obj)
+
+#Send objects to Speckle API
+r = s.ObjectCreateAsync(objects)
+assert r is not None and r['success'] == True, "Could not create Speckle Objects"
+
+#Grab object Ids from response
+obj_ids = [resource['_id'] for resource in r['resources']]
+
+
+stream = {
+    "owner": username,
+    "private": False,
+    "anonymousComments": True,
+    "canRead": [],
+    "canWrite": [],
+    "comments": [],
+    "deleted": False,
+    "name": "sofistik_run",
+    "objects": [],
+    "layers": [],
+    "baseProperties": {},
+    "globalMeasures": {},
+    "isComputedResult": False,
+    "viewerLayers": [],
+    "parent": "string",
+    "children": [],
+    "ancestors": []
+}
+
+#Create stream
+r = s.StreamCreateAsync(stream)
+assert r is not None and r['success'] == True, "Could not create Speckle Stream"
+
+stream_id = r['resource']['_id']
+
+#Add objects to stream
+r = s.StreamAddObjectsAsync(stream_id, obj_ids)
